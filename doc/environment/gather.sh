12288635b506de507a03c425ff47ac4419b50374
Martin Ueding <dev@martin-ueding.de>
2014-01-07 08:14:02 +0100

    Change order of commands in script

--- doc/environment/gather.sh
+++ doc/environment/gather.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Copyright © 2014 Martin Ueding <dev@martin-ueding.de>
 
+env | sort > env-user.txt
+sudo env | sort > env-root.txt
 sudo su -c env mu | sort > env-su.txt
 sudo su -c 'env env' mu | sort > env-su_env.txt
-sudo env | sort > env-root.txt
-env | sort > env-user.txt
 
+diff -u env-user.txt env-root.txt > diff-user-root.diff
 diff -u env-user.txt env-su.txt > diff-user-su.diff
 diff -u env-user.txt env-su_env.txt > diff-user-su_env.diff
-diff -u env-user.txt env-root.txt > diff-user-root.diff
