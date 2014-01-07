2f35a0479d7228c38c4d64b1954d023a9bcc9df0
Martin Ueding <dev@martin-ueding.de>
2014-01-07 09:51:26 +0100

    Update copyright years

--- doc/conf.py
+++ doc/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'think-rotate'
-copyright = '2012-2013, Martin Ueding and Jim Turner'
+copyright = '2012-2014, Martin Ueding and Jim Turner'
 
 try:
     sys.path.append('..')
