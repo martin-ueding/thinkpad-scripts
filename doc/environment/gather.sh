ed83008ae3a125037e1d2dd31da654eda18891e7
Martin Ueding <dev@martin-ueding.de>
2014-01-07 08:08:45 +0100

    New diffs

--- doc/environment/gather.sh
+++ doc/environment/gather.sh
@@ -8,3 +8,7 @@ sudo su -c env mu | sort > env-su.txt
 sudo su -c 'env env' mu | sort > env-su_env.txt
 sudo env | sort > env-root.txt
 env | sort > env-user.txt
+
+diff -u env-user.txt env-su.txt > diff-user-su.diff
+diff -u env-user.txt env-su_env.txt > diff-user-su_env.diff
+diff -u env-user.txt env-root.txt > diff-user-root.diff
