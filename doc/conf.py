f7dbc73a0d4091f061f2c651afbe5fffc3c238a5
Martin Ueding <dev@martin-ueding.de>
2014-02-20 19:45:39 +0100

    State kernel dependency

--- doc/conf.py
+++ doc/conf.py
@@ -249,3 +249,7 @@ texinfo_documents = [
 
 # How to display URL addresses: 'footnote', 'no', or 'inline'.
 #texinfo_show_urls = 'footnote'
+
+rst_epilog = '''
+.. |project| replace:: think-rotate
+'''
