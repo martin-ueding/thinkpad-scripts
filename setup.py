0cf2e0f5d25ba31469c53e688eead83707d53e2c
Martin Ueding <dev@martin-ueding.de>
2014-03-14 23:44:48 +0100

    Add more

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Copyright © 2014 Martin Ueding <dev@martin-ueding.de>
+
+from setuptools import setup, find_packages
+
+if __name__ == '__main__':
+    packages = find_packages()
+
+    print(packages)
+
+    setup(
+        author = "Martin Ueding",
+        author_email = "dev@martin-ueding.de",
+        description = "Scripts for ThinkPad®",
+        license = "GPL2",
+        classifiers=[
+            "Environment :: Console",
+            "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+            "Programming Language :: Python",
+
+        ],
+        name = "thinkpad-scripts",
+        packages = packages,
+        entry_points = {
+            'console_scripts': [
+                'thinkpad-config = tps/config:main',
+                'thinkpad-dock = tps/dock:main',
+                'thinkpad-rotate = tps/rotate:main',
+            ],
+        },
+        install_requires=[
+            'termcolor',
+        ],
+        url = "https://github.com/martin-ueding/thinkpad-scripts",
+        download_url="http://martin-ueding.de/download/thinkpad-scripts/",
+        version = "4.0",
+    )
---
+++ tps/network.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright © 2014 Martin Ueding <dev@martin-ueding.de>
+# Licensed under The GNU Public License Version 2 (or later)
+
+'''
+Network support.
+'''
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+def set_wifi(on):
+    '''
+    Sets the wifi hardware to the given state.
+    '''
+    logger.error('set_wifi() not implemented')
+
+def restart(connection):
+    '''
+    Disables and enables the given connection if it exists.
+    '''
+    logger.error('restart() not implemented')
