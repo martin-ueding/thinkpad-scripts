d7f621413ba69c6e1d0f506753a8922e148280bd
Martin Ueding <dev@martin-ueding.de>
2014-03-15 14:50:57 +0100

    Fix spacing

--- setup.py
+++ setup.py
@@ -9,32 +9,32 @@ if __name__ == '__main__':
     packages = find_packages()
 
     setup(
-        author = "Martin Ueding",
-        author_email = "dev@martin-ueding.de",
-        description = "Scripts for ThinkPad®",
-        license = "GPL2",
+        author="Martin Ueding",
+        author_email="dev@martin-ueding.de",
+        description="Scripts for ThinkPad®",
+        license="GPL2",
         classifiers=[
             "Environment :: Console",
             "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
             "Programming Language :: Python",
 
         ],
-        name = "thinkpad-scripts",
-        packages = packages,
-        entry_points = {
+        name="thinkpad-scripts",
+        packages=packages,
+        entry_points={
             'console_scripts': [
-                'thinkpad-config = tps.config:main',
-                'thinkpad-dock = tps.dock:main',
-                'thinkpad-rotate = tps.rotate:main',
-                'thinkpad-touch = tps.input:main_touchscreen',
-                'thinkpad-touchpad = tps.input:main_touchpad',
-                'thinkpad-trackpoint = tps.input:main_trackpoint',
+                'thinkpad-config=tps.config:main',
+                'thinkpad-dock=tps.dock:main',
+                'thinkpad-rotate=tps.rotate:main',
+                'thinkpad-touch=tps.input:main_touchscreen',
+                'thinkpad-touchpad=tps.input:main_touchpad',
+                'thinkpad-trackpoint=tps.input:main_trackpoint',
             ],
         },
         install_requires=[
             'termcolor',
         ],
-        url = "https://github.com/martin-ueding/thinkpad-scripts",
+        url="https://github.com/martin-ueding/thinkpad-scripts",
         download_url="http://martin-ueding.de/download/thinkpad-scripts/",
-        version = "4.0",
+        version="4.0",
     )
