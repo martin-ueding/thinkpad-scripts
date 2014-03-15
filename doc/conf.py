227042a13e5dff9a86a9fd8655f667b323ad3f4d
Martin Ueding <dev@martin-ueding.de>
2014-03-15 12:43:40 +0100

    Implement toggle()

--- doc/conf.py
+++ doc/conf.py
@@ -21,6 +21,11 @@ extensions = [
     'sphinx.ext.autodoc',
 ]
 
+autodoc_default_flags = [
+    'members',
+    'undoc-members',
+]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
--- tps/vkeyboard.py
+++ tps/vkeyboard.py
@@ -4,9 +4,36 @@
 # Copyright © 2014 Martin Ueding <dev@martin-ueding.de>
 # Licensed under The GNU Public License Version 2 (or later)
 
+'''
+Logic for virtual keyboard
+'''
+
 import logging
+import subprocess
 
 logger = logging.getLogger(__name__)
 
-def toggle(state):
-    logger.error('toggle() not implemented')
+def toggle(program, state):
+    '''
+    Toggles the running state of the given progam.
+
+    If state is true, the program will be spawned.
+
+    :param str program: Name of the program
+    :param bool state: Desired state
+    :returns: None
+    '''
+    if state:
+        command = ['pgrep', program]
+        try:
+            logger.debug(command)
+            subprocess.check_output(command)
+        except subprocess.CalledProcessError:
+            command = program + '&'
+            logger.debug(command)
+            subprocess.check_call(command, shell=True)
+    else:
+        command = ['killall', program]
+        logger.debug(' '.join(command))
+        subprocess.check_call(command)
+
