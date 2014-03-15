a37af0f684b6281c41210aa2e0a4872741065cc1
Martin Ueding <dev@martin-ueding.de>
2014-03-15 13:42:33 +0100

    Implement set_wifi()

--- tps/network.py
+++ tps/network.py
@@ -10,16 +10,28 @@ Network support.
 
 import logging
 
+import tps
+
 logger = logging.getLogger(__name__)
 
 def set_wifi(on):
     '''
     Sets the wifi hardware to the given state.
+
+    :param bool on: Desired state
+    :returns: None
     '''
-    logger.error('set_wifi() not implemented')
+    if not tps.has_program('nmcli'):
+        logger.warning('nmcli is not installed')
+        return
+
+    tps.check_call(['nmcli', 'nm', 'wifi', 'on' if on else 'off'], logger)
 
 def restart(connection):
     '''
     Disables and enables the given connection if it exists.
+
+    :param str connection: Name of the connection
+    :returns: None
     '''
     logger.error('restart() not implemented')
