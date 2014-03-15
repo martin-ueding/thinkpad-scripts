5743c61e1936f9b679e6a0ab03ddc5b3dab67702
Martin Ueding <dev@martin-ueding.de>
2014-03-15 13:28:19 +0100

    More documentation

--- tps/config.py
+++ tps/config.py
@@ -4,6 +4,12 @@
 # Copyright © 2014 Martin Ueding <dev@martin-ueding.de>
 # Licensed under The GNU Public License Version 2 (or later)
 
+'''
+Config module.
+
+Takes care of the INI style config file for global and user configuration.
+'''
+
 import configparser
 import os.path
 
@@ -13,6 +19,15 @@ CONFIGFILE = os.path.expanduser('~/.config/thinkpad-scripts/config.ini')
 'Path of global config file'
 
 def get_config():
+    '''
+    Loads the config from the config files.
+
+    The global config file is read first, then the user config file is read.
+    That way, options can be overwritten in the user config file.
+
+    :returns: Config
+    :rtype: configparser.ConfigParser
+    '''
     config = configparser.ConfigParser()
 
     config.read('default.ini')
@@ -22,6 +37,12 @@ def get_config():
     return config
 
 def print_config(config):
+    '''
+    Pretty prints config with colors.
+
+    :param configparser.ConfigParser config: Config to print
+    :returns: None
+    '''
     for section in sorted(config.sections()):
         termcolor.cprint(section, attrs=('bold',))
 
@@ -29,6 +50,11 @@ def print_config(config):
             print(termcolor.colored(key, 'yellow'), config[section][key])
 
 def main():
+    '''
+    Command line entry point.
+
+    :returns: None
+    '''
     print_config(get_config())
 
 if __name__ == '__main__':
