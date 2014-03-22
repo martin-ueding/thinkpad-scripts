f4936c288e4408c390138708a50482e43d130837
Martin Ueding <dev@martin-ueding.de>
2014-03-23 00:44:45 +0100

    Take over functionality of rotate-hook

--- tps/hooks.py
+++ tps/hooks.py
@@ -4,8 +4,12 @@
 # Copyright © 2014 Martin Ueding <dev@martin-ueding.de>
 # Licensed under The GNU Public License Version 2 (or later)
 
+import argparse
 import logging
 import os.path
+import re
+import subprocess
+import sys
 
 import tps
 
@@ -58,3 +62,30 @@ def postdock(state, config):
     hook = os.path.expanduser(config['hooks']['postdock'])
     if os.path.isfile(hook):
         tps.call([hook, 'on' if state else 'off'], logger)
+
+def get_graphicsl_user():
+    pattern = re.compile(r'\(:0(\.0)?\)')
+
+    lines = tps.check_output(['who', '-u'], logger).decode().split('\n')
+    for line in lines:
+        m = pattern.search(line)
+        if m:
+            words = line.split()
+            return words[0]
+
+def main_rotate_hook():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('key', help='Keycode')
+    options = parser.parse_args()
+
+    key = options.key
+
+    if key in ('00000001', '00005009'):
+        set_to = ''
+
+    elif key in ('00000000', '0000500a'):
+        set_to = 'normal'
+
+    command = 'su -c "env DISPLAY=:0.0 /usr/bin/thinkpad-rotate {set_to}" --login "{user}" & disown'.format(user=get_graphicsl_user(), set_to=set_to)
+    logger.debug('subprocess “{}”'.format(command))
+    subprocess.call(command, shell=True)
