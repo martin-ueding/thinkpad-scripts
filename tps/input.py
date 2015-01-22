caec26914f94c4b689da80dea671b73e218a2f84
Martin Ueding <dev@martin-ueding.de>
2015-01-22 08:48:53 +0100

    PEP8 compliance

--- tps/input.py
+++ tps/input.py
@@ -17,11 +17,13 @@ import tps.config
 
 logger = logging.getLogger(__name__)
 
+
 class InputDeviceNotFoundException(Exception):
     '''
     ``xinput`` device could not be found.
     '''
     pass
+
 
 def get_wacom_device_ids():
     '''
@@ -48,6 +50,7 @@ def get_wacom_device_ids():
             ids.append(int(matcher.group(1)))
     return ids
 
+
 def rotate_wacom_device(device, direction):
     '''
     Rotates a Wacom® device.
@@ -57,6 +60,7 @@ def rotate_wacom_device(device, direction):
     '''
     tps.check_call(['xsetwacom', 'set', str(device), 'rotate',
                     direction.xsetwacom], logger)
+
 
 def map_wacom_device_to_output(device, output):
     '''
@@ -68,6 +72,7 @@ def map_wacom_device_to_output(device, output):
     tps.check_call(['xsetwacom', 'set', str(device), 'MapToOutput', output],
                    logger)
 
+
 def rotate_all_wacom_devices(direction):
     '''
     Rotates all Wacom® devices.
@@ -75,12 +80,14 @@ def rotate_all_wacom_devices(direction):
     for device in get_wacom_device_ids():
         rotate_wacom_device(device, direction)
 
+
 def map_all_wacom_devices_to_output(output):
     '''
     Maps all Wacom® devices.
     '''
     for device in get_wacom_device_ids():
         map_wacom_device_to_output(device, output)
+
 
 def get_xinput_id(name):
     '''
@@ -100,6 +107,7 @@ def get_xinput_id(name):
     raise InputDeviceNotFoundException(
         'Input device “{}” could not be found'.format(name))
 
+
 def set_xinput_state(device, state):
     '''
     Sets the device state.
@@ -113,6 +121,7 @@ def set_xinput_state(device, state):
     tps.check_call(['xinput', 'set-prop', str(device), 'Device Enabled',
                     set_to], logger)
 
+
 def get_xinput_state(device):
     '''
     Gets the device state.
@@ -123,7 +132,8 @@ def get_xinput_state(device):
     :rtype: bool
     '''
     output = tps.check_output(['xinput', '--list', str(device)], logger)
-    return not b'disabled' in output
+    return b'disabled' not in output
+
 
 def set_wacom_touch(device_id, state):
     '''
@@ -131,6 +141,7 @@ def set_wacom_touch(device_id, state):
     '''
     tps.check_call(['xinput', 'set-prop', str(device_id), 'Wacom Enable Touch',
                     '1' if state else '0'], logger)
+
 
 def state_change_ui(config_name):
     '''
