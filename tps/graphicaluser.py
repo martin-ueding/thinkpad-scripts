e1c970b310ed3cf301264bc509c757244206ac8f
Martin Ueding <dev@martin-ueding.de>
2015-07-31 16:08:34 +0200

    Start with loginctl method

--- tps/graphicaluser.py
+++ tps/graphicaluser.py
@@ -9,7 +9,7 @@
 Attempts to find the currently logged in user using the graphical session.
 '''
 
-import argparse
+import configparser
 import logging
 import os.path
 import re
@@ -29,6 +29,7 @@ def get():
     until a sensible result is found.
     '''
     methods = [
+        _get_loginctl,
         _get_who,
         _get_pgrep,
     ]
@@ -36,6 +37,7 @@ def get():
     for method in methods:
         result = method()
         if result is not None:
+            logger.info('Found a sensible user with %s.', method.__name__)
             return result
 
     return None
@@ -96,5 +98,42 @@ def _get_pgrep():
 
     return uid_str
 
+
+def _is_loginctl_user_active(user):
+    output = tps.check_output(['loginctl', 'show-user', user, '--property=State'], logger)
+    line = output.decode().strip()
+
+    return line == 'State=active'
+
+
+def _loginctl_users():
+    output = tps.check_output(['loginctl', 'list-users', '--no-legend'], logger)
+    lines = output.decode().split('\n')
+
+    uid_name_pattern = re.compile(r'(?P<uid>\d+)\s*(?P<name>\S+)')
+
+    users = []
+
+    for line in lines:
+        matcher = uid_name_pattern.search(line)
+        if matcher:
+            group_dict = matcher.groupdict()
+            users.append(group_dict['name'])
+
+    return users
+
+
+def _get_loginctl():
+    users = _loginctl_users()
+    active = [user for user in users if _is_loginctl_user_active(user)]
+
+    if len(active) == 1:
+        return active[0]
+    else:
+        logging.debug('Users determined active by loginctl are: %s', ', '.join(active))
+        return None
+
+
 if __name__ == '__main__':
+    tps.config.set_up_logging(2)
     print(get())
